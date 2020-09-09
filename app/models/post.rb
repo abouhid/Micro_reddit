47d301ee4e7d6260adb8a7400b905c2758371671
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-09 13:15:09 -0300

    apply rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   validates :title, length: { in: 8..50, message: 'Title should have between 8 and 50 characters.' }
-  validates :body, length: { in: 50..500, message: 'Body should have between 50 and 500 characters.'}
+  validates :body, length: { in: 50..500, message: 'Body should have between 50 and 500 characters.' }
   belongs_to :author
   has_many :comments
 end
