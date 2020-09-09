ee31ddd81b25a6422cf354fc978f954c10bd127b
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-09 13:13:18 -0300

    Apply linters

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,7 +2,7 @@ class Author < ApplicationRecord
   PASSWORD_REQUIREMENTS = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x.freeze
   validates :username, presence: true, uniqueness: true
   validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
-                              message: 'invalid email format' }
+                                                message: 'invalid email format' }
   validates :password, format: PASSWORD_REQUIREMENTS
   validates_associated :posts, :comments
   has_many :posts
