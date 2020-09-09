e92d5d4646e92c5750056c0ecdde8542747c1715
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-09 11:59:16 -0300

    author validation created

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,11 @@
 class Author < ApplicationRecord
+  PASSWORD_REQUIREMENTS = /\A(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/x.freeze
+  validates :username, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+                              message: 'invalid email format' }
+  validates :password, format: PASSWORD_REQUIREMENTS
+  validates_associated :posts
+  validates_associated :comments
   has_many :posts
   has_many :comments
 end
